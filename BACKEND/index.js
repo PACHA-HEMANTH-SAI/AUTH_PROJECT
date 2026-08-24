7bfc24fe5421bcd20beb94c086472c3980dd5c29
Hemanth Sai <pachahemanthsai@gmail.com>
2026-08-24 20:59:23 +0530

    updated typo in index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -20,7 +20,7 @@ app.use('/api/auth', authRoutes);
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, '/FRONTEND/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "FRONTEND", "dist", "index.html"));
     })
 }
 
