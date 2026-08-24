b091f1d43c1861d0b5ecef88c031ee30b58abc22
Hemanth Sai <pachahemanthsai@gmail.com>
2026-08-24 20:57:12 +0530

    updated to production level code

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
@@ -8,17 +9,20 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 app.use(cors({origin : 'http://localhost:5173', credentials : true}));
 app.use(express.json());
 app.use(cookieParser());
-//allows us to parse incomming requests with json payloads
-
-
-app.get('/', (req,res) => {
-    res.send("Hello World!...");
-});
 
 app.use('/api/auth', authRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/FRONTEND/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT,() => {
     connectDB();
